5b9002f11c21aff99ab541d2683b209227a02ba7
maennis <mark.andrew.ennis@gmail.com>
2017-09-07 16:49:33 -0700

    Added Student class and testing index page

---
+++ Student.php
@@ -0,0 +1,35 @@
+<?php
+
+class Student {
+
+    function _construct() {
+        $this->surname='';
+        $this->firstname='';
+        $this->emails = array();
+        $this->grades = array();
+    }
+
+    function add_email($which, $email) {
+        $this->emails[$which] = $email;
+    }
+
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
+
+    function calculate_average() {
+        $total = 0;
+        foreach($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+
+    function toString() {
+        $result = $this->firstname . ' ' . $this->surname;
+        $result .= ' (' . $this->average() . ")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': ' . $what . "\n";
+        $result .= "\n";
+        return '<pre>' . $result . '<\pre>';
+    }
+}
